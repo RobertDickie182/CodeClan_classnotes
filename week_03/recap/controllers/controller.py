b5c00de17f52e73e378f6caea257d71c6f3571ed
Jarrod Bennie <jarrodbennie@icloud.com>
2023-06-20 09:17:12 +0100

    Add week_03/recap code

--- week_03/recap/controllers/controller.py
+++ week_03/recap/controllers/controller.py
@@ -6,3 +6,8 @@ from models.book_list import books
 @app.route("/books")
 def index():
     return render_template("index.html", books=books)
+
+@app.route("/books/<index>")
+def show(index):
+    print(f"the index is {index}")
+    return render_template("show.html", book=books[int(index)])
