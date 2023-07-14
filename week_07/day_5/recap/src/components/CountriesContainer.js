a5542ef400ba65f6192211cf7274351d33b85b74
Jarrod Bennie <jarrodbennie@icloud.com>
2023-07-14 14:57:11 +0100

    Add week_07/day_5/recap code

--- week_07/day_5/recap/src/components/CountriesContainer.js
+++ week_07/day_5/recap/src/components/CountriesContainer.js
@@ -1,8 +1,15 @@
 import React, {useState, useEffect} from "react";
+import CountryInfo from "./CountryInfo";
+import FavouriteCountriesList from "./FavouriteCountriesList";
+import Select from "./Select";
 
 function CountriesContainer() {
     const [countries, setCountries] = useState([]);
+    const [favouriteCountries, setFavouriteCountries] = useState([]);
+    const [selectedCountry, setSelectedCountry] = useState(null);
 
+
+    // es6 - .then()
     // function fetchCountries() {
     //     fetch('https://restcountries.com/v3.1/all')
     //         .then(response => response.json())
@@ -10,6 +17,7 @@ function CountriesContainer() {
     //         .catch(err => console.error(err));
     // }
 
+    // es7 - async / await
     async function fetchCountries() {
         try {
             const response = await fetch('https://restcountries.com/v3.1/all');
@@ -24,17 +32,35 @@ function CountriesContainer() {
         fetchCountries();
     }, []);
 
+    function updateSelectedCountry(cca3) {
+        const newSelectedCountry = countries.find(country => {
+            return country.cca3 === cca3;
+        });
+        setSelectedCountry(newSelectedCountry);
+    }
+
+    function addFavouriteCountry(newFavouriteCountry) {
+        const updatedFavouriteCountries = [...favouriteCountries, newFavouriteCountry];
+        setFavouriteCountries(updatedFavouriteCountries);
+    }
+
+    const countriesSelectData = countries.map(country => {
+        return {
+            text: country.name.common,
+            value: country.cca3
+        };
+    });
+
     return (
         <>
             <h1>Countries App</h1>
-            <select>
-                <option value="" key="">Afghanistan</option>
-            </select>
-            <section>
-                <ul>
-                    <li>Population: 123456</li>
-                </ul>
-            </section>
+            <Select
+                data={countriesSelectData}
+                onChange={updateSelectedCountry} />
+            <CountryInfo country={selectedCountry}
+                onFavouriteButtonClick={addFavouriteCountry}/>
+            <FavouriteCountriesList
+                countries={favouriteCountries}/>
         </>
     );
 }
